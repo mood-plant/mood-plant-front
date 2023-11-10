a42cff5a87aa43f4ba0d647a884c0d36f04ca729
LJH <049494@ewhain.net>
2023-11-11 02:05:29 +0900

    feat : react-toast 팝업 추가

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './style/theme';
-import { StyledEngineProvider } from '@mui/styled-engine';
-import Main from './pages/Main';
-import SelectItem from './pages/SelectItem';
-import CheckResult from './pages/CheckResult';
-import Complete from './pages/Complete';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./style/theme";
+import { StyledEngineProvider } from "@mui/styled-engine";
+import Main from "./pages/MainPage";
+import SelectItem from "./pages/SelectItem";
+import CheckResult from "./pages/CheckResult";
+import Complete from "./pages/Complete";
 import Credit from "./pages/Credit";
 import Loading from "./pages/Loading";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <div className='App'>
-          <Routes>
-            <Route path={'/selectItem'} element={<SelectItem />} />
-            <Route path={'/'} element={<Main />} />
-            <Route path={'/checkResult'} element={<CheckResult />} />
-            <Route path={'/complete'} element={<Complete />} />
-            <Route path={"/credit"} element={<Credit />} />
-            <Route path={"/loading"} element={<Loading />} />
-          </Routes>
-        </div>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <>
+      <Toaster />
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <div className="App">
+            <Routes>
+              <Route path={"/selectItem"} element={<SelectItem />} />
+              <Route path={"/"} element={<Main />} />
+              <Route path={"/checkResult"} element={<CheckResult />} />
+              <Route path={"/complete"} element={<Complete />} />
+              <Route path={"/credit"} element={<Credit />} />
+              <Route path={"/loading"} element={<Loading />} />
+            </Routes>
+          </div>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </>
   );
 }
 
