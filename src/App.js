922e57f4eff3d91ef31d8e935e702802be1882a1
y00eunji <aine0227@naver.com>
2023-11-10 11:37:40 +0900

    feat: 헤더 삭제

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './style/theme';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import Main from './pages/Main';
-import Header from './components/Common/Header';
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <div className='App'>
-          <Header />
           <Routes>
             <Route path={'/'} element={<Main />} />
           </Routes>
