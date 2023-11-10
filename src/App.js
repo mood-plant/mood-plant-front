869875ec206dbdc042ed1ced50458799b61fa7b3
LJH <049494@ewhain.net>
2023-11-10 12:22:41 +0900

    feat : 공용 헤더 완성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './style/theme';
-import { StyledEngineProvider } from '@mui/styled-engine';
-import Main from './pages/Main';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./style/theme";
+import { StyledEngineProvider } from "@mui/styled-engine";
+import Main from "./pages/Main";
+import SelectItem from "./pages/SelectItem";
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
-        <div className='App'>
+        <div className="App">
           <Routes>
-            <Route path={'/'} element={<Main />} />
+            <Route path={"/"} element={<Main />} />
+          </Routes>
+          <Routes>
+            <Route path={"/selectItem"} element={<SelectItem />} />
           </Routes>
         </div>
       </ThemeProvider>
--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,16 +1,51 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import right from "../../assets/Header/right.png";
+import close from "../../assets/Header/close.png";
+import logo from "../../assets/Header/logo.png";
 
-export default function Header() {
-  return <S.Header>감성플랜트</S.Header>;
+export default function Header({ number }) {
+  return (
+    <S.Container>
+      <S.Logo src={logo} />
+      <S.ProgressContainer>
+        <S.Progress highlight={number === "1"}>결과 확인</S.Progress>
+        <S.Arrow src={right} />
+        <S.Progress highlight={number === "2"}>상품 선택</S.Progress>
+        <S.Arrow src={right} />
+        <S.Progress highlight={number === "3"}>확인 및 결제</S.Progress>
+      </S.ProgressContainer>
+      <S.Close src={close} />
+    </S.Container>
+  );
 }
 
 const S = {
-  Header: styled.header`
-    color: #1a4346;
-    font-weight: 400;
+  Container: styled.div`
     display: flex;
-    justify-content: flex-start;
+    align-items: center;
+    justify-content: space-between;
     padding-left: 48px;
+    padding-right: 48px;
     padding-top: 48px;
+  `,
+  Logo: styled.img`
+    width: 120px;
+  `,
+  ProgressContainer: styled.div`
+    display: flex;
+    align-items: center;
+  `,
+  Progress: styled.p`
+    color: ${(props) => (props.highlight ? "#000000" : "#888888")};
+    font-weight: ${(props) => (props.highlight ? 700 : 400)};
+    text-decoration: ${(props) => (props.highlight ? "underline" : "none")};
+    font-size: 20px;
+  `,
+  Arrow: styled.img`
+    width: 29.16px;
+  `,
+  Close: styled.img`
+    width: 30px;
+    padding-left: 90px;
   `,
 };
