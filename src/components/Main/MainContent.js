483c1aea174855d494556697a813f056c710cd2f
y00eunji <aine0227@naver.com>
2023-11-11 01:23:42 +0900

    feat: 로딩 페이지 이동

--- src/components/Main/MainContent.js
+++ src/components/Main/MainContent.js
@@ -5,9 +5,12 @@ import { postLink } from '../../api/Main/postLink';
 import { useNavigate } from 'react-router-dom';
 import Cards from './Cards';
 import Images from './Images';
+import Loading from '../Common/Loading';
+import LogoHeader from '../Common/LogoHeader';
 
 export default function MainContent() {
   const [link, setLink] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const onChangeLink = (event) => {
@@ -15,7 +18,11 @@ export default function MainContent() {
   };
 
   const onSendUrl = async () => {
+    setIsLoading(true);
+
     const url = await postLink(link);
+
+    setIsLoading(false);
 
     navigate('/checkResult', {
       state: {
@@ -31,70 +38,81 @@ export default function MainContent() {
   };
 
   return (
-    <S.Layout>
-      <Typography
-        variant='h3'
-        component='h3'
-        sx={{
-          fontWeight: 700,
-          marginBottom: '10px',
-        }}
-      >
-        내 공간을 더 특별하게
-      </Typography>
-      <Typography
-        variant='subtitle1'
-        component='div'
-        sx={{ fontWeight: 500, marginBottom: '100px' }}
-      >
-        감성플랜트는 당신의 공간에 최적화된 조경식물들을 제안합니다.
-        <br />
-        플렌테리어를 통해 공간을 업그레이드하고 더 많은 방문객들을 모으세요.
-      </Typography>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <LogoHeader />
 
-      <S.URL>
-        <S.InputWrapper>
-          <input
-            type='text'
-            placeholder='https://beanbrothers.com'
-            value={link}
-            onChange={onChangeLink}
-            onKeyPress={handleKeyPress}
-            style={{
-              width: '640px',
-              borderRadius: '20px',
-              border: '2px solid #808080',
-              background: '#FAFAFA',
-              height: '60px',
-              outline: 0,
-              fontSize: '20px',
-              paddingLeft: '20px',
-            }}
-          />
+          <S.Layout>
+            <Typography
+              variant='h3'
+              component='h3'
+              sx={{
+                fontWeight: 700,
+                marginBottom: '10px',
+              }}
+            >
+              내 공간을 더 특별하게
+            </Typography>
+            <Typography
+              variant='subtitle1'
+              component='div'
+              sx={{ fontWeight: 500, marginBottom: '100px' }}
+            >
+              감성플랜트는 당신의 공간에 최적화된 조경식물들을 제안합니다.
+              <br />
+              플렌테리어를 통해 공간을 업그레이드하고 더 많은 방문객들을
+              모으세요.
+            </Typography>
 
-          <button
-            onClick={onSendUrl}
-            style={{
-              height: '60px',
-              marginLeft: '23px',
-              width: '200px',
-              fontSize: '20px',
-              borderRadius: '20px',
-              background: '#1A4346',
-              outline: 0,
-              border: '2px solid #1A4346',
-              color: 'white',
-            }}
-          >
-            시작하기
-          </button>
-        </S.InputWrapper>
-      </S.URL>
-      <Images />
-      <S.CardLayout>
-        <Cards />
-      </S.CardLayout>
-    </S.Layout>
+            <S.URL>
+              <S.InputWrapper>
+                <input
+                  type='text'
+                  placeholder='https://beanbrothers.com'
+                  value={link}
+                  onChange={onChangeLink}
+                  onKeyPress={handleKeyPress}
+                  style={{
+                    width: '640px',
+                    borderRadius: '20px',
+                    border: '2px solid #808080',
+                    background: '#FAFAFA',
+                    height: '60px',
+                    outline: 0,
+                    fontSize: '20px',
+                    paddingLeft: '20px',
+                  }}
+                />
+
+                <button
+                  onClick={onSendUrl}
+                  style={{
+                    height: '60px',
+                    marginLeft: '23px',
+                    width: '200px',
+                    fontSize: '20px',
+                    borderRadius: '20px',
+                    background: '#1A4346',
+                    outline: 0,
+                    border: '2px solid #1A4346',
+                    color: 'white',
+                  }}
+                >
+                  시작하기
+                </button>
+              </S.InputWrapper>
+            </S.URL>
+            <Images />
+            <S.CardLayout>
+              <Cards />
+            </S.CardLayout>
+          </S.Layout>
+        </>
+      )}
+    </>
   );
 }
 
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MainContent from '../components/Main/MainContent';
 import styled from 'styled-components';
-import LogoHeader from '../components/Common/LogoHeader';
 
 export default function Main() {
   return (
     <S.Layout>
-      <LogoHeader />
       <MainContent />
     </S.Layout>
   );
